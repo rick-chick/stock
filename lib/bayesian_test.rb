8b63070639b5dbc2cd50d641ec056a9b2e355a74
AkishigeTakehiro <so1itaryrove@gmail.com>
2015-01-14 17:06:04 +0900

    fix bayesian_test.rb to scoring algorithm

--- lib/bayesian_test.rb
+++ lib/bayesian_test.rb
@@ -1,15 +1,16 @@
 require File.expand_path(File.dirname(__FILE__)) + '/require.rb'
 
-to       = Date.latest
-from     = to.prev(3000)
-closes   = Daily.closes(from ,to, code:1301)
-log      = closes.fill_blank.log
-kmeans   = Cluster::KMeans.new(log.values, 4)
-bayesian = Bayesian::K2Metric.new(3)
+to        = Date.latest
+from      = to.prev(3000)
+closes    = Daily.closes(from ,to, code:1301)
+log       = closes.fill_blank.log
+grid_size = 4
+cluster   = Cluster::KMeans.new(log.values, grid_size)
+bayesian  = Bayesian::K2Metric.new(4)
 
-kmeans.clusterize
+cluster.clusterize
 grd_log = log.calc(1) do |stocks|
-  kmeans.clusters.find do |cluster|
+  cluster.clusters.find do |cluster|
     cluster.enclose?(Cluster::Node[stocks[0].value])
   end
 end
@@ -19,14 +20,18 @@ bayesian.each do |node|
     grd_log.delay(parent_rank)
   end
   cpt = {}
-  Stocks.merge(grd_log, *delays) do |stocks|
-    key = stocks.map {|stock| stock.value.center}
+  Stocks.merge(grd_log.delay(node.rank), *delays) do |stocks|
+    key = stocks.map {|stock| stock.value.center if stock}
     next if key.include? nil
     cpt[key] ||= 0
     cpt[key] += 1
   end
-  Bayesian::K2Metric.score(cpt)
+  bayesian.score(cpt, node.parents.length, grid_size)
 end
 
-puts bayesian.nodes
-
+bayesian.nodes.each do |node|
+  puts node.rank
+  node.parents.each do |parent|
+    puts " #{parent}"
+  end
+end
