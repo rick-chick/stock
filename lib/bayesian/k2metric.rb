19c84c31a9bd8785ebdf494d6e50b044c53a6b0e
AkishigeTakehiro <so1itaryrove@gmail.com>
2015-01-14 17:03:33 +0900

    fix bayesian\k2memtric.rb to add score method

--- lib/bayesian/k2metric.rb
+++ lib/bayesian/k2metric.rb
@@ -10,8 +10,8 @@ module Bayesian
 
     def each(&block)
       (0...@num_nodes).each {|n| @nodes << Bayesian::Node.new(n)}
-      best = 0
       @nodes.each do |child|
+        best = -Float::MAX
         score = block.call(child)
         best  = [score, best].max
         (child.rank+1...@num_nodes).each do |parent_rank|
@@ -24,6 +24,50 @@ module Bayesian
           end
         end
       end
+    end
+
+    #
+    # @ijk [Hash]     cpt 
+    # @q_i [Integer]  number of node i's parents
+    # @r_i [Integer]  number of node i's states
+    #
+    #key   is child and parents value set 
+    #value is count of the child and parents value set
+    #
+    #child1:    node1 is child
+    #parent2:   node2 is parent
+    #child1(0): child1 have 0's value
+    #
+    #1) child have some parents
+    # key                                value
+    #  [child1(0), parent2(0),  ...  ]    32
+    #  [child1(0), parent2(1), ...   ]    11
+    #  [child1(1), parent2(0), ...   ]    2
+    # 
+    #2) child have no parents
+    # key                                value
+    #  [child1(0)]                        46
+    #  [child1(1)]                        2
+    #  [child1(2)]                        12
+    #
+    #note 
+    # cpt(ijk) can take only one child node and some parents of the child
+    # ie. i is fixed. j,k are vary
+    def score(ijk, q_i, r_i)
+      ij = {}
+      ijk.each do |key, n_ijk|
+        p_i = key[1..q_i]
+        ij[p_i] ||= 0
+        ij[p_i] += n_ijk
+      end
+      ret = q_i*Math.log(Math.gamma(r_i))
+      ij.each  do |key, n_ij| 
+        (1...r_i+n_ij).each {|m| ret -= Math.log(m)}
+      end
+      ijk.each do |key, n_ijk| 
+        (1...1+n_ijk).each {|m| ret += Math.log(m)}
+      end
+      ret
     end
   end
 end
