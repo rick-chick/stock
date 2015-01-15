a3f8d7439f8348c2248a63a1410a5e2cbd62946a
akishige <so1itaryrove@gmail.com>
2015-01-16 07:33:08 +0900

    fix bayesian_test.rb to puts results

--- lib/bayesian_test.rb
+++ lib/bayesian_test.rb
@@ -2,11 +2,11 @@ require File.expand_path(File.dirname(__FILE__)) + '/require.rb'
 
 to        = Date.latest
 from      = to.prev(3000)
-closes    = Daily.closes(from ,to, code:1301)
+closes    = Daily.closes(from ,to, code: 9984)
 log       = closes.fill_blank.log
-grid_size = 4
+grid_size = t3
 cluster   = Cluster::KMeans.new(log.values, grid_size)
-bayesian  = Bayesian::K2Metric.new(4)
+bayesian  = Bayesian::K2Metric.new(10)
 
 cluster.clusterize
 grd_log = log.calc(1) do |stocks|
@@ -26,7 +26,9 @@ bayesian.each do |node|
     cpt[key] ||= 0
     cpt[key] += 1
   end
-  bayesian.score(cpt, node.parents.length, grid_size)
+  score = bayesian.score(cpt, grid_size)
+  puts "#{node.rank} #{node.parents} #{score}"
+  score
 end
 
 bayesian.nodes.each do |node|
