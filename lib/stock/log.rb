c686edd081bbcfc96a82f2f54e99dd226c178aac
akishige <so1itaryrove@gmail.com>
2015-02-04 07:14:49 +0900

    fix some bug,relate to directory and to_s

--- lib/stock/log.rb
+++ lib/stock/log.rb
@@ -1,15 +1,15 @@
 class Log
 
   def initialize(file)
-    @path = "#{File.dirname(File.expand_path('../../log/', __FILE__))}/#{file}_#{Time.Now.strftime('%Y%m%d%H%M%S')}"
+    @path = "#{File.expand_path('../../../log/', __FILE__)}/#{file}_#{Time.now.strftime('%Y%m%d%H%M%S')}"
     dir  = File.dirname(@path)
     FileUtils.mkdir_p dir if not File.exists? dir
   end
 
   def puts(line)
-    STDOUT << line + "\n"
+    STDOUT << "#{line}\n"
     File.open(@path, 'a') do |file|
-      file << line + "\n"
+      file << "#{line}\n"
     end
   end
 end
