755f64ecc199f36f158ceafa801f91fc422520db
AkishigeTakehiro <so1itaryrove@gmail.com>
2015-07-08 18:03:17 +0900

    fix

--- example/trade_example.rb
+++ example/trade_example.rb
@@ -10,23 +10,25 @@ point = nil
 board.watch do |boards|
   board2 = boards.find {|a| a.code == 1568}
   board1 = boards.find {|a| a.code == 1579}
-  buy   = board1.buy - board2.sell + 20
-  sell  = board1.sell - board2.buy - 20
-  value = board1.price - board2.price
-  point = buy if buy == sell
-  db = buy - point if point
-  ds = sell - point if point
-  disps = [value, buy, sell, db, ds]
-  if not prev == disps
-    pair = Pair.new
-    pair.key = PairTime.new(1568, 1579, Time.now)
-    pair.close = value
-    pair.high = sell
-    pair.low = buy
-    pair.open = point
-    pair.insert
-    p pair
+  if board1 and board2
+    buy   = board1.buy - board2.sell + 20
+    sell  = board1.sell - board2.buy - 20
+    value = board1.price - board2.price
+    point = buy if buy == sell
+    db = buy - point if point
+    ds = sell - point if point
+    disps = [value, buy, sell, db, ds]
+    if not prev == disps
+      pair = Pair.new
+      pair.key = PairTime.new(1568, 1579, Time.now)
+      pair.close = value
+      pair.high = buy
+      pair.low = sell
+      pair.open = point
+      pair.insert
+      p pair
+    end
+    prev = disps
   end
-  prev = disps
   true
 end
