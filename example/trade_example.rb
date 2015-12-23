f02fca8a8640d9065473246a15a4ed5ff2e4a495
AkishigeTakehiro <so1itaryrove@gmail.com>
2015-12-23 15:41:53 +0900

    update

--- example/trade_example.rb
+++ example/trade_example.rb
@@ -10,59 +10,64 @@ prev = nil
 point = nil
 player = Player.new
 player.ticks = [10, 10]
-player.volumes = [20, 20]
+player.volumes = [10, 10]
 player.codes = codes
 agent = SmbcStock.new
 agent.log_in ARGV[2], ARGV[3], ARGV[4]
 player.hands = agent.hands
 player.orders = agent.orders
 invalid_orders = []
+t = Time.now
+break_start_time = Time.new(t.year, t.month, t.day, 11, 30, 0)
+break_end_time = Time.new(t.year, t.month, t.day, 12, 30, 0)
 board.watch do |boards|
-  board1 = boards.find {|a| a.code == codes[0]}
-  board2 = boards.find {|a| a.code == codes[1]}
-  if board1 and board2
-    buy   = board1.buy - board2.sell + 20
-    sell  = board1.sell - board2.buy - 20
-    value = board1.price - board2.price
-    point = buy if buy == sell
-    disps = [value, buy, sell]
-    if not prev == disps
-      pair = Pair.new
-      pair.key = PairTime.new(codes[0], codes[1], Time.now)
-      pair.close = value
-      pair.high = buy
-      pair.low = sell
-      pair.open = point
-      pair.insert
-    end
-    prev = disps
-
-    player.boards = boards
-    if player.have_uncontracted_order?
-      player.hands = agent.hands
-      player.orders = agent.orders
-    else
-      invalid_orders = []
-    end
-
-    tmp = []
-    invalid_orders.each do |order|
-      result = agent.recept(order)
-      if result.orderd?
-        result.insert
-      else
-        tmp << order
+  if not (break_start_time <= Time.now and Time.now <= break_end_time)
+    board1 = boards.find {|a| a.code == codes[0]}
+    board2 = boards.find {|a| a.code == codes[1]}
+    if board1 and board2
+      buy   = board1.buy - board2.sell + 20
+      sell  = board1.sell - board2.buy - 20
+      value = board1.price - board2.price
+      point = buy if buy == sell
+      disps = [value, buy, sell]
+      if not prev == disps
+        pair = Pair.new
+        pair.key = PairTime.new(codes[0], codes[1], Time.now)
+        pair.close = value
+        pair.high = buy
+        pair.low = sell
+        pair.open = point
+        pair.insert
       end
-    end
-    invalid_orders = tmp
-    invalid_orders.sort! {|a, b| a <=> b}
+      prev = disps
 
-    player.decide do |order|
-      result = agent.recept(order)
-      if result.orderd?
-        result.insert
+      player.boards = boards
+      if player.have_uncontracted_order?
+        player.hands = agent.hands
+        player.orders = agent.orders
       else
-        invalid_orders << order
+        invalid_orders = []
+      end
+
+      tmp = []
+      invalid_orders.each do |order|
+        result = agent.recept(order)
+        if result.orderd?
+          result.insert
+        else
+          tmp << order
+        end
+      end
+      invalid_orders = tmp
+      invalid_orders.sort! {|a, b| a <=> b}
+
+      player.decide do |order|
+        result = agent.recept(order)
+        if result.orderd?
+          result.insert
+        else
+          invalid_orders << order
+        end
       end
     end
   end
